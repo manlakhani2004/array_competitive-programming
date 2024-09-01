ce9b728dfa5efce9669378e56b1851f010c5ede6
manlakhani2004 <126983888+manlakhani2004@users.noreply.github.com>
2024-09-01 11:55:00 +0530

    Time: 7 ms (85.56%), Space: 13.1 MB (45.42%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.cpp
@@ -3,19 +3,31 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        vector<int> ans;
-        for(int i=0; i<sentences.size(); i++)
-        {
-            string str = sentences[i];
-            stringstream ss(str);
-            string token;
-            vector<string> tokens;
+        // vector<int> ans;
+        // for(int i=0; i<sentences.size(); i++)
+        // {
+        //     string str = sentences[i];
+        //     stringstream ss(str);
+        //     string token;
+        //     vector<string> tokens;
 
-            while (getline(ss, token, ' ')) {
-                tokens.push_back(token);
+        //     while (getline(ss, token, ' ')) {
+        //         tokens.push_back(token);
+        //     }
+        //     ans.push_back(tokens.size());
+        //  }             
+        // return *max_element(ans.begin(),ans.end()); 
+        vector<int> ans;
+        for(int i=0; i<sentences.size(); i++){
+            int count =1;
+            for(int j=0; j<sentences[i].size(); j++)
+            {
+                if(sentences[i][j]==' '){
+                    count++;
+                }
             }
-            ans.push_back(tokens.size());
-         }             
-        return *max_element(ans.begin(),ans.end());    
+            ans.push_back(count);
+        }   
+        return *max_element(ans.begin(),ans.end()); 
     }
 };

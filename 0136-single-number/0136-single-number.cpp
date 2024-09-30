0b64bba1790f075c0751823fe3068dfa2c3b0fff
manlakhani2004 <126983888+manlakhani2004@users.noreply.github.com>
2024-09-30 08:10:46 +0530

    Time: 26 ms (14.16%), Space: 23.7 MB (16.33%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -2,20 +2,42 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n=nums.size();
-        int count;
+        //bruote solution
+        // int count;
+        // int ans;
+        // for(int i=0; i<n; i++){
+        //     count=0;
+        //     for(int j=0; j<n; j++){
+        //         if(nums[i]==nums[j]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count==1){
+        //         ans = nums[i];
+        //         break;
+        //     }
+        // }   
+        // return ans;
+
+        //batter solution
+        unordered_map<int,int> hash;
         int ans;
         for(int i=0; i<n; i++){
-            count=0;
-            for(int j=0; j<n; j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+            hash[nums[i]]++;
+        }
+
+        for(auto it:hash){
+            if(it.second == 1){
+                ans  = it.first;
             }
-            if(count==1){
-                ans = nums[i];
-                break;
-            }
-        }   
+        }
         return ans;
+
+        //optimal solution
+        // int number = 0;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     number ^= nums[i];
+        // }
+        // return number;
     }
 };

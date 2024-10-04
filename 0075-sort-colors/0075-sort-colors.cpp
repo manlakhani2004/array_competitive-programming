5ba0bc4e55ea0026f6b57fac0376fead77d380cc
manlakhani2004 <126983888+manlakhani2004@users.noreply.github.com>
2024-10-04 22:00:49 +0530

    Time: 0 ms (100%), Space: 10.3 MB (33.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i=0; i<nums.size()-1; i++){
-            int min=i;
-            for(int j=i; j<nums.size(); j++){
-                if(nums[j]<nums[min])
-                {
-                    min = j;
-                }
+        int zero=0,one=0,two=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==0){
+                zero++;
             }
-            int temp = nums[i];
-            nums[i] = nums[min];
-            nums[min] = temp;
+            else if(nums[i]==1){
+                one++;
+            }
+            else{
+                two++;
+            }
         }
+        if(nums.size()>1){
+        for(int i=0; i<zero; i++){
+            nums[i]=0;
+        }
+        for(int i=zero; i<zero+one; i++){
+            nums[i]=1;
+        }
+        for(int i=zero+one; i<nums.size(); i++){
+            nums[i]=2;
+        }
+        }
+
+        // for(int i=0; i<nums.size()-1; i++){
+        //     int min=i;
+        //     for(int j=i; j<nums.size(); j++){
+        //         if(nums[j]<nums[min])
+        //         {
+        //             min = j;
+        //         }
+        //     }
+        //     int temp = nums[i];
+        //     nums[i] = nums[min];
+        //     nums[min] = temp;
+        // }
     }
 };

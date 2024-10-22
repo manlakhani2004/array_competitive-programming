099ddee920c9b90e6ab3514a11a0d8dd416c91df
manlakhani2004 <126983888+manlakhani2004@users.noreply.github.com>
2024-10-22 22:44:27 +0530

    Time: 1326 ms (6.34%), Space: 295.9 MB (5.78%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,59 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
+        set<vector<int>> hashset;
         int n = nums.size();
         sort(nums.begin(),nums.end());
-
-        for(int i=0; i<n; i++)
-        {
-            if(i>0 && nums[i]==nums[i-1])continue;
+        for(int i=0; i<n; i++){
             int j=i+1;
             int k=n-1;
+
             while(j<k){
                 int sum = nums[i]+nums[j]+nums[k];
-                if(sum>0){
+                if(sum<0){
+                    j++;
+                }else if(sum>0){
                     k--;
-                }else if(sum<0){
-                   j++; 
                 }else{
-                    vector<int> temp={nums[i],nums[j],nums[k]};
-                    ans.push_back(temp);
+                    vector<int> temp = {nums[i],nums[j],nums[k]};
+                    hashset.insert(temp);
                     j++;
                     k--;
-                    while(j<k&& nums[j]==nums[j-1])j++;
-                    while(j<k && nums[k]==nums[k+1])k--;
                 }
             }
         }
+
+        vector<vector<int>>ans;
+        for(vector<int> li:hashset){
+            ans.push_back(li);
+        }
         return ans;
+        // vector<vector<int>> ans;
+        // int n = nums.size();
+        // sort(nums.begin(),nums.end());
+
+        // for(int i=0; i<n; i++)
+        // {
+        //     if(i>0 && nums[i]==nums[i-1])continue;
+        //     int j=i+1;
+        //     int k=n-1;
+        //     while(j<k){
+        //         int sum = nums[i]+nums[j]+nums[k];
+        //         if(sum>0){
+        //             k--;
+        //         }else if(sum<0){
+        //            j++; 
+        //         }else{
+        //             vector<int> temp={nums[i],nums[j],nums[k]};
+        //             ans.push_back(temp);
+        //             j++;
+        //             k--;
+        //             while(j<k&& nums[j]==nums[j-1])j++;
+        //             while(j<k && nums[k]==nums[k+1])k--;
+        //         }
+        //     }
+        // }
+        // return ans;
 
     }
 };
